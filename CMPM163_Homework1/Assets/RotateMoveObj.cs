7a47f4e5523c07b5d9effa99c9ae01924b417590
Terry DuBois <tdubois@ucsc.edu>
2019-04-16 14:38:32 -0700

    HW 1A init

---
+++ CMPM163_Homework1/Assets/RotateMoveObj.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMoveObj : MonoBehaviour
+{
+    public bool rotate;
+    public float rotateSpeed = 10f;
+    public bool move;
+    public float moveSpeed = 10f;
+
+    void Start()
+    {
+        InvokeRepeating("flipMoveSpeed", 2.0f, 2.0f);
+    }
+
+    void Update()
+    {
+        if (rotate) {
+            transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+        }
+
+        if (move) {
+            transform.position = new Vector3(
+                transform.position.x + moveSpeed,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+    }
+
+    void flipMoveSpeed()
+    {
+        moveSpeed *= -1;
+    }
+}
