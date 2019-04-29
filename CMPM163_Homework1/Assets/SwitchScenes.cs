a25a0fda73bf49dae76e9e0df6577623d1a06464
Terry DuBois <tdubois@ucsc.edu>
2019-04-28 17:18:04 -0700

    Cycle between scenes

---
+++ CMPM163_Homework1/Assets/SwitchScenes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScenes : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            if (sceneName == "Part A") {
+                SceneManager.LoadScene("Part B");
+            }
+            else if (sceneName == "Part B") {
+                SceneManager.LoadScene("Part C");
+            }
+            else if (sceneName == "Part C") {
+                SceneManager.LoadScene("Part A");
+            }
+        }
+    }
+}
