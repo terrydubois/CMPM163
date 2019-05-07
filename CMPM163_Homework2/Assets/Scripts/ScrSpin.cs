661cdf72c67536b6b64ae0bbd03b7b34963cefce
Terry DuBois <tdubois@ucsc.edu>
2019-05-07 11:26:01 -0700

    Car spins

---
+++ CMPM163_Homework2/Assets/Scripts/ScrSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrSpin : MonoBehaviour
+{
+
+    public float spinSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0,  Time.deltaTime * spinSpeed, Space.Self);
+    }
+}
