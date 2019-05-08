5acacc12e7a56426493eb5b86dc9ae6e6a582a1f
Terry DuBois <tdubois@ucsc.edu>
2019-05-08 13:05:03 -0700

    Sliders for RGB

--- CMPM163_Homework2/Assets/Scripts/ScrCycleColor.cs
+++ CMPM163_Homework2/Assets/Scripts/ScrCycleColor.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class ScrCycleColor : MonoBehaviour
 {
+    Renderer render;
+    public float colorR;
+    public float colorG;
+    public float colorB;
+
+    public void sliderChangedR(float newVal)
+    {
+        colorR = newVal;
+    }
+    public void sliderChangedG(float newVal)
+    {
+        colorG = newVal;
+    }
+    public void sliderChangedB(float newVal)
+    {
+        colorB = newVal;
+    }
 
     void Start()
     {
-        
+        render = GetComponent<Renderer>();
+        render.material.shader = Shader.Find("Custom/Outline");
+        colorR = 1;
+        colorG = 1;
+        colorB = 1;
     }
 
     void Update()
     {
-        
+        Vector4 newColor = new Vector4(colorR, colorG, colorB, 1);
+        render.material.SetVector("_OutlineColor", newColor);
     }
 }
