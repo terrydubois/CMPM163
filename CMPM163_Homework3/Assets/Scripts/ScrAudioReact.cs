c33d8a575ab62a81dcf746360e2eb9106ec2aa6f
Terry DuBois <36257165+terrydubois@users.noreply.github.com>
2019-05-22 23:59:19 -0700

    Audio Light

---
+++ CMPM163_Homework3/Assets/Scripts/ScrAudioReact.cs
@@ -0,0 +1,58 @@
+﻿// Credit for this script to Brian Hansens
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrAudioReact : MonoBehaviour
+{
+    public Light lt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // --------------------------------------------------------
+		// ------- animate the cube size based on spectrum data.
+
+		// consolidate spectral data to 8 partitions (1 partition for each rotating cube)
+		int numPartitions = 8;
+		float[] aveMag = new float[numPartitions];
+		float partitionIndx = 0;
+		int numDisplayedBins = 512 / 2; //NOTE: we only display half the spectral data because the max displayable frequency is Nyquist (at half the num of bins)
+
+		for (int i = 0; i < numDisplayedBins; i++) 
+		{
+			if(i < numDisplayedBins * (partitionIndx + 1) / numPartitions){
+				aveMag[(int)partitionIndx] += ScrAudioPeer.spectrumData [i] / (512/numPartitions);
+			}
+			else{
+				partitionIndx++;
+				i--;
+			}
+		}
+
+		// scale and bound the average magnitude.
+		for(int i = 0; i < numPartitions; i++)
+		{
+			aveMag[i] = (float)0.5 + aveMag[i]*100;
+			if (aveMag[i] > 100) {
+				aveMag[i] = 100;
+			}
+		}
+
+		// Map the magnitude to the cubes based on the cube name.
+        //transform.localScale = new Vector3 (aveMag[0], aveMag[0], aveMag[0]);
+        lt.intensity = aveMag[0];
+
+
+		// --------- End animating cube via spectral data
+		// --------------------------------------------------------
+
+    }
+}
